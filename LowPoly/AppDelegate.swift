4722977391959180ce96f55aa4f61f0b9a162612
Ben Murphy <ben@constellation.software>
2017-03-30 16:55:22 -0400

    Initial Commit

---
+++ LowPoly/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  LowPoly
+//
+//  Created by Ben Murphy on 3/30/17.
+//  Copyright (c) 2017 Constellation Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
